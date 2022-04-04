50d313d47ff03a0e313768cbc16091ec2433da84
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-04-04 16:50:01 +0200

    Update language_app.py

--- language_app.py
+++ language_app.py
@@ -4,10 +4,15 @@ from googletrans import Translator
 st.title("My translator")
 translator = Translator()
 
-word = st.text_input("Write a word to translate:", "insert yourr word here")
-destlang = st.selectbox('Which language do you choose?',
-('af', 'sq', 'ar', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'el', 'ht', 'haw', 'he', 'hi', 'is', 'ga', 'it', 'ko', 'no', 'pl', 'sk', 'sv', 'uz', 'vi', 'yi'))
-abc = translator.translate(text=word, dest=destlang) 
-#st.balloons()
-st.write("You chose a word in", abc.src, "and the", destlang, "translation is:")
-st.info(abc.text)
+word = st.text_input("Write a word to translate:", "insert your word here")
+if word != ' ':
+  srclang = st.selectbox("In which language is the word you chose?",
+  ('af', 'sq', 'ar', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'el', 'ht', 'haw', 'he', 'hi', 'is', 'ga', 'it', 'ko', 'no', 'pl', 'sk', 'sv', 'uz', 'vi', 'yi'))
+  destlang = st.selectbox('In which language would you like to translate it?',
+  ('af', 'sq', 'ar', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'el', 'ht', 'haw', 'he', 'hi', 'is', 'ga', 'it', 'ko', 'no', 'pl', 'sk', 'sv', 'uz', 'vi', 'yi'))
+  abc = translator.translate(src=srclang, text=word, dest=destlang) 
+  #st.balloons()
+  st.write("You chose a word in", abc.src, "and the", destlang, "translation is:")
+  st.info(abc.text)
+else:
+  pass
