7400e77bbc1d5de8b82b93582dc3d06212d1853f
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-04-04 16:29:14 +0200

    Update language_app.py

--- language_app.py
+++ language_app.py
@@ -9,4 +9,5 @@ destlang = st.selectbox('Which language do you choose?',
 ('it', 'en', 'de', 'cs', 'ru', 'fr', 'es', 'zu', 'hu', 'uk', 'sk'))
 abc = translator.translate(text=word, dest=destlang) 
 st.balloons()
-st.write("You chose a word in", abc.src, "and the", destlang, "translation is", abc.text)
+st.write("You chose a word in", abc.src, "and the", destlang, "translation is:")
+st.info(abc.text)
