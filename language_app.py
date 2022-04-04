323ed268d7d7e2669520f845e36d558e7a754368
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-04-04 16:45:43 +0200

    Update language_app.py

--- language_app.py
+++ language_app.py
@@ -8,6 +8,6 @@ word = st.text_input("Write a word to translate:", "insert yourr word here")
 destlang = st.selectbox('Which language do you choose?',
 ('af', 'sq', 'ar', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'el', 'ht', 'haw', 'he', 'hi', 'is', 'ga', 'it', 'ko', 'no', 'pl', 'sk', 'sv', 'uz', 'vi', 'yi'))
 abc = translator.translate(text=word, dest=destlang) 
-st.balloons()
+#st.balloons()
 st.write("You chose a word in", abc.src, "and the", destlang, "translation is:")
 st.info(abc.text)
