f3f41563ad7d9749f688554430d75d94816bcd8c
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-04-04 16:20:37 +0200

    Update language_app.py

--- language_app.py
+++ language_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
+from googletrans import Translator
 
+st.title("My translator")
+translator = Translator()
+
+word = text_input("gimme a word to translate, or write 'nothing' to stop me: ")
+destlang = st.selectbox('Which language do you choose?',
+('it', 'en', 'de', 'cs', 'ru', 'fr', 'es', 'zu', 'hu', 'uk', 'sk'))
+abc = translator.translate(text=word, dest=destlang) 
+st.balloons()
+st.write("You chose a word in", abc.src, "and the", destlang, "translation is", abc.text)
