b1e5989ca4d7a7b1087efc86b71517be8012b0b8
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-04-04 16:53:47 +0200

    Update language_app.py

--- language_app.py
+++ language_app.py
@@ -11,7 +11,7 @@ if word != ' ':
   destlang = st.selectbox('In which language would you like to translate it?',
   ('af', 'sq', 'ar', 'ca', 'hr', 'cs', 'da', 'nl', 'en', 'eo', 'et', 'tl', 'fi', 'fr', 'el', 'ht', 'haw', 'he', 'hi', 'is', 'ga', 'it', 'ko', 'no', 'pl', 'sk', 'sv', 'uz', 'vi', 'yi'))
   abc = translator.translate(src=srclang, text=word, dest=destlang) 
-  st.balloons()
+  #st.balloons()
   st.write("You chose a word in", abc.src, "and the", destlang, "translation is:")
   st.info(abc.text)
 else:
